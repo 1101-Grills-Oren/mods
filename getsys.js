6c6a5775dc6f0b82a8ca6f4ea40d563beed5e35c
Nero <134662565+1101-Grills-Oren@users.noreply.github.com>
2025-09-05 13:00:00 -0700

    Update getsys.js

--- getsys.js
+++ getsys.js
@@ -1,1 +1,13 @@
-console.log(fetch("https://raw.githubusercontent.com/1101-Grills-Oren/mods/refs/heads/main/"))
+const octokit = new Octokit({
+  auth: 'github_p'+'at_11BADMTJI0dmSDa8V5xAJ3_9wUUK5VYsAq'+'jNY6nndJ5rDlTVwuLIYFGGVHzvWEGAQwHFQOVRMDH7yB9C86'
+})
+
+await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+  owner: '1101-Grills-Oren',
+  repo: 'mods',
+  path: '',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+//console.log(fetch("https://raw.githubusercontent.com/1101-Grills-Oren/mods/refs/heads/main/"))
